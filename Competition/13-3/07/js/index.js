b6c2e5c116eb79b23a42fe40b163f39dce87e319
47641 <476412065@qq>
2023-03-06 22:54:22 +0800

    test

--- Competition/13-3/07/js/index.js
+++ Competition/13-3/07/js/index.js
@@ -1,6 +1,16 @@
 
- function getweather() {
-     //TODO：请补充代码
+function getweather() {
+    let data = $.getJSON('./js/weather.json').responseJSON;
+    console.log(data);
+    data.unshift(data.pop());
+    for (let i = 0; i < 7; i++) {
+        $(`.item:nth-of-type(${i}) img`).src(data[i].weather_icon);
+        $(`.item-mess:nth-of-type(${i}) div:nth-of-type(1)`).text(data[i].weather);
+        $(`.item-mess:nth-of-type(${i}) div:nth-of-type(2)`).text(data[i].temperature);
+        $(`.item-mess:nth-of-type(${i}) div:nth-of-type(3)`).text(data[i].winp);
+        $(`.item-mess:nth-of-type(${i}) span:nth-of-type(1)`).text(data[i].days);
+        $(`.item-mess:nth-of-type(${i}) span:nth-of-type(2)`).text(data[i].weak);
+    }
 }
 
 getweather();
