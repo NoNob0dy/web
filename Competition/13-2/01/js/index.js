d0c030fd41fb5ac70ad401b518ce849faafa00ce
den21s <476412065@qq.com>
2023-03-09 19:25:54 +0800

    test

--- Competition/13-2/01/js/index.js
+++ Competition/13-2/01/js/index.js
@@ -3,6 +3,21 @@
  * @param {Object} num
  * */
 const splitArray = (oldArr, num) => {
-  // TODO：请补充代码实现功能
+  oldArr = oldArr.sort((x, y) => {
+    if (x < y) {
+      return -1;
+    } else if (x > y) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  let len = oldArr.length,
+    newArr = new Array();
+  for (let i = 0; i < len; i += num) {
+    if (i + num >= len) { newArr.push(oldArr.slice(i, len)); }
+    else { newArr.push(oldArr.slice(i, i + num)); }
+  }
+  return newArr;
 };
 module.exports = splitArray; // 检测需要，请勿删除
--- Competition/13-2/04/js/index.js
+++ Competition/13-2/04/js/index.js
@@ -1,1 +1,55 @@
-// TODO：请补充代码
+// 2h30min TODO：请补充代码
+var process = '';
+const formula = document.getElementById("formula"),
+    result = document.getElementById("result"),
+    calbtn = document.getElementsByClassName("calc-button"),
+    cals = ['x', '÷', '+', '-', '√'];
+
+for (let i = 0; i < calbtn.length; i++) {
+    let cal = calbtn[i],
+        text = cal.innerHTML;
+    if (text == "AC") {
+        cal.onclick = () => {
+            result.value = '';
+            formula.value = '';
+            process = '';
+        }
+    } else if (text == '=') {
+        cal.onclick = () => {
+            process = process.replace('x', '*');
+            process = process.replace('÷', '/');
+            result.value = eval(process);
+            process = '';
+        }
+    } else if (text == '√') {
+        cal.onclick = () => {
+            preserve(text);
+            let res = Math.sqrt(eval(process));
+            if (res != Math.floor(res)) {
+                result.value = "NaN";
+            } else {
+                result.value = res;
+            }
+            process = '';
+        }
+    }
+    else {
+        cal.onclick = () => {
+            preserve(text);
+            formula.value += text;
+            process += text;
+        }
+    }
+}
+
+function preserve(text) {
+    if (process == '') {
+        formula.value = '';
+        if (cals.includes(text)) {
+            formula.value += result.value;
+            process += result.value;
+        }
+    }
+}
+
+
