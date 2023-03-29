74a34acc3286b50ef1d93cfa0097d45646e8f60d
den21s <476412065@qq.com>
2023-03-29 20:33:38 +0800

    test

--- Competition/13-2/06/js/index.js
+++ Competition/13-2/06/js/index.js
@@ -1,19 +1,63 @@
 $(function () {
-  // 使用 ajax 获取 userList.json 数据并渲染到页面
   getData();
-
-  // 为按钮添加事件
+  let rl = [], ll = $("#leftSelect option").map(item => $(item));
+  console.log(ll);
   $("#add").click(function () {
-    // TODO：补充代码，实现功能
+    let change = [];
+    for (let i = 0; i < ll.length; i++) {
+      if (ll[i].attr("selected")) {
+        ll[i].attr("selected", false);
+        $('#rightSelect').append(ll[i]);
+        ll[i].remove();
+        
+        rl.push(ll[i]);
+        change.push(ll[i]);
+        ll.splice(i, 1);
+      }
+    }
+    changeAccess(true, change);
   });
   $("#addAll").click(function () {
-    // TODO：补充代码，实现功能
+    let change = [];
+    for (let i = 0; i < ll.length; i++) {
+
+      $('#rightSelect').append(ll[i]);
+      ll[i].remove();
+
+      rl.push(ll[i]);
+      change.push(ll[i]);
+      ll.splice(i, 1);
+    }
+    changeAccess(true, change);
   });
   $("#remove").click(function () {
-    // TODO：补充代码，实现功能
+    let change = [];
+    for (let i = 0; i < rl.length; i++) {
+      if (rl[i].attr("selected")) {
+
+        rl[i].attr("selected", false);
+        $('#leftSelect').append(rl[i]);
+        rl[i].remove();
+
+        ll.push(rl[i]);
+        change.push(rl[i]);
+        rl.splice(i, 1);
+      }
+    }
+    changeAccess(false, change);
   });
   $("#removeAll").click(function () {
-    // TODO：补充代码，实现功能
+    let change = [];
+    for (let i = 0; i < rl.length; i++) {
+
+      $('#leftSelect').append(rl[i]);
+      rl[i].remove();
+
+      ll.push(rl[i]);
+      change.push(ll[i]);
+      rl.splice(i, 1);
+    }
+    changeAccess(false, change);
   });
 });
 
@@ -23,9 +67,44 @@ $(function () {
  * @param {Object} changeList 要修改权限的用户列表
  */
 function changeAccess(right, changeList) {
-  // TODO：补充代码，实现功能
+  const names = $("#userList td:even").map((item) => $(item)),
+        rights = $("#userList td:odd").map((item) => $(item));  
+  for (let i = 1, cnt = 0; cnt < changeList.length; i++) {
+    const val = changeList[cnt++].value;
+    if (names[i].text() == val) {
+      if (right) {
+        rights[i].text("管理员");
+      } else {
+        rights[i].text("普通用户");
+      }
+    }
+  }
 }
-// 异步获取数据
+
 function getData() {
-  // TODO：补充代码，实现功能
+  const tab = $("#userList");
+  $.getJSON("./js/userList.json").then(res => {
+    for (key in res) {
+      let tr = $("<tr>");
+      appendName(tr, res[key].name);
+      appendRight(tr, res[key].right)
+      tab.append(tr);
+    }
+  })
+}
+
+function appendName(tr, name) {
+  let td = $("<td>");
+  td.text(name);
+  tr.append(td);
+}
+
+function appendRight(tr, right) {
+  let td = $("<td>");
+  if (right) {
+    td.text("管理员");
+  } else {
+    td.text("普通用户");
+  }
+  tr.append(td);
 }
