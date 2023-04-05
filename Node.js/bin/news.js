74c6838a7a7620b6461e5d3b1a4bf37607e5f791
47641 <476412065@qq>
2023-04-05 18:48:52 +0800

    test

--- Node.js/bin/news.js
+++ Node.js/bin/news.js
@@ -1,14 +1,8 @@
-// TODO: 待补充代码
-var http = require("http");
-
-http.get('/news', (res) => {
-    res.setEncoding('utf8');
-})
-
-const server = http.createServer((req, res) => {
+var http = require('http');
+http.createServer((req, res) => {
     res.setHeader("Content-type", "text/html;charset=utf8");
-    res.end(JSON.stringify(
-        [
+    if (req.url == '/news') {
+        res.end(JSON.stringify([
             {
                 "channelId": "5572a108b3cdc86cf39001cd",
                 "name": "国内焦点"
@@ -17,11 +11,9 @@ const server = http.createServer((req, res) => {
                 "channelId": "5572a108b3cdc86cf39001ce",
                 "name": "国际焦点"
             }
-        ]
-    ))
-});
+        ]))
+    } else {
+        res.end('404');
+    }
 
-server.listen(8080, () => {
-    console.log("listening in: http://localhost:8080");
-})
-
+}).listen(8080)
