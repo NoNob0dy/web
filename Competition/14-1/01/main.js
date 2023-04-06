0841d246ba7fea9a555c77f48bbc5bb7d59c9e5c
den21s <476412065@qq.com>
2023-04-06 11:33:01 +0800

    test

---
+++ Competition/14-1/01/main.js
@@ -0,0 +1,15 @@
+/**
+ * @description: 数据类型检测
+ * @param {*} data 传入的待检测数据
+ * @return {*} 返回数据类型
+ */
+function getType(data) {
+  // TODO：待补充代码
+
+}
+
+module.exports = {
+  getType
+}
+
+
---
+++ Competition/14-1/02/index.js
@@ -0,0 +1,7 @@
+const inputs = document.querySelectorAll(".controls input");
+
+/**
+ * 上面已经选取了两个取色器
+ * 请添加相应的 JS 事件处理函数并绑定到合适的事件监听器上（提示：change 事件）
+ * 这样我们就可以用取色器选取颜色来生成下方的渐变色背景啦
+ *  */
