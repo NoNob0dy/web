f8dd79ffec7d8aa8cbde62af492ecac45865f9f9
den21s <476412065@qq.com>
2023-06-09 18:11:00 +0800

    test

--- Competition/13-2/09/js/index.js
+++ Competition/13-2/09/js/index.js
@@ -3,7 +3,42 @@
  * @return {*} 根据传入的数据生成对应的 js 格式数据
  */
 let generateData = (data) => {
-  // TODO：待补充代码
+   let res = [];
+   let cnt = 1;
+   let json = JSON.stringify(data);
+   let repeat = json.match(/\"\{\{repeat\((\d+(\,\d+)?)\)\}\}\"/);
+   if (repeat !== null) {
+      json = json.substring(2 + repeat[0].length, json.length - 1);
+      cnt = getRandom(repeat[0]);
+   }
+   for (let i = 0; i < cnt; i++) {
+      res.push(parseJson(json));
+   }
+   return res;
 };
+
+function parseJson(json) {
+   let replace = json;
+   let bool = /\"\{\{bool\(\)\}\}\"/;
+   let integer = /\"\{\{integer\((\d+(\,\d+)?)\)\}\}\"/;
+
+   if (replace.match(bool) !== null) {
+      replace = replace.replace(bool, getRandom("0, 1") == 1 ? true : false);
+   }
+   let tmp = replace.match(integer)
+   if (tmp !== null) {
+      replace = replace.replace(integer, getRandom(tmp[0]));
+   }
+   return JSON.parse(replace);
+}
+
+function getRandom(str) {
+   let num = str.match(/\d+(\,\d+)?/)[0].split(",").map(str => parseInt(str));
+   if (num.length == 1) {
+      return num[0];
+   } else {
+      return Math.round(Math.random() * (num[1] - num[0]) + num[0]);
+   }
+}
 
 module.exports = { generateData };
