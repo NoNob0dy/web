fc2d3016de9ef02892d1fb7e39997d0b27fead09
47641 <476412065@qq>
2023-06-09 22:07:00 +0800

    test

--- Competition/13-3/10/index.js
+++ Competition/13-3/10/index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: false }))
 
 
 //添加用户
-app.post('/add', function(req, res) {
+app.get('/add', function(req, res) {
     fs.readFile(path.resolve(__dirname,'./users.json'), 'utf8', function(err, data) {
         data = JSON.parse(data);
         data["userlist"].push({
